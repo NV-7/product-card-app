d128d95feea6c17991833061197752e835b88ee7
Tommy <naviboun@gmail.com>
2025-08-17 16:20:04 -0500

    Added Dark and light mode button and styling for the ProductCard component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import ProductCard from "./components/ProductCard";
+import React, { createContext } from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App(){
 
-var price = [100,200,300,400,500,600,700,800,900,1000];
+  const [darkMode, setDarkMode] = useState('light');
 
+  const toggleDarkMode = () => {
+    setDarkMode((curr) => curr === 'dark' ? 'light' : 'dark');
+  };
+  
+const urlImages = [
+  "https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg",
+  "https://images.unsplash.com/photo-1484788984921-03950022c9ef?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bGFwdG9wfGVufDB8fDB8fHww",
+  "https://images.unsplash.com/photo-1561154464-82e9adf32764?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGFibGV0fGVufDB8fDB8fHww",
+  "https://images.unsplash.com/photo-1588200908342-23b585c03e26?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1vbml0b3J8ZW58MHx8MHx8fDA%3D",
+  "https://images.unsplash.com/photo-1527814050087-3793815479db?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vdXNlfGVufDB8fDB8fHww"
+];    
 
 const products =  [
-    {name: "Phone", price: 100, img: "https://png.pngtree.com/png-vector/20241203/ourmid/pngtree-mobile-phone-frame-photo-png-image_14568670.png", inStock: true},
-    {name: "Laptop", price: 200, img: "https://via.placeholder.com/150", inStock: true},
-    {name: "Tablet", price: 300, img: "https://via.placeholder.com/150", inStock: false},
-    {name: "Monitor", price: 400, img: "https://via.placeholder.com/150", inStock: true},
-    {name: "Mouse", price: 500, img: "https://via.placeholder.com/150", inStock: true},
+    {name: "Phone", price: 500, img: urlImages[0], inStock: true},
+    {name: "Laptop", price: 200, img: urlImages[1], inStock: true},
+    {name: "Tablet", price: 300, img: urlImages[2], inStock: false},
+    {name: "Monitor", price: 400, img: urlImages[3], inStock: true},
+    {name: "Mouse", price: 500, img: urlImages[4], inStock: true},
 ]
 
   return (
-   <div>
-    {products.map((product) => <ProductCard product={product} />)}
-    </div>
+   <div id={darkMode} className="app-wrapper">
+    <div className="button"> </div>
+    <button onClick={toggleDarkMode}>
+       {darkMode === 'dark' ? 'Light' : 'Dark'} 
+    </button>
+      <div className= "product-list" >
+      {products.map((product) => <ProductCard product={product} />)}
+      </div>
+   </div>
+    
   );
 
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,20 +3,22 @@ import '../css/ProductCard.css';
 
 function ProductCard({product}) {
 
-    
     return (
-
-        
     <div className="product-card"> 
+
         <div className ="product-image">
             <img src={product.img} alt={product.name} />
         </div>
-        <div className="product-name">
-            <h2>{product.name}</h2>
-            <p>{product.inStock === true ? product.price: "Not in Stock"}</p> 
+
+        <div className= 'product-details'> 
+            < div className="product-name">
+                <h2>{product.name}</h2>
+            </div>
+            <div className="product-price">
+                <h3> {product.inStock === true ? 'Price: $' + product.price: "Not in Stock"}</h3> 
+            </div>
         </div>
-        <div className="product-price">
-         </div>
+
      </div>
     )
 }
